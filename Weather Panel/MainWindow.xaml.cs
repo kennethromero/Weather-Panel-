67407462de81ccaf0fb5e93d20566f5382fe867d
John Kenneth Romero <jeromero@csmbataan.ph.education>
2020-11-19 16:49:32 +0400

    created button

--- Weather Panel/MainWindow.xaml.cs
+++ Weather Panel/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Weather_Panel
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
