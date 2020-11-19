6ec4264c6866fc5d14fcd97958bddacbfc202a13
John Kenneth Romero <jeromero@csmbataan.ph.education>
2020-11-19 17:38:58 +0400

    Created WeatherArea class

---
+++ Weather Panel/WeatherArea.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather_Panel
+{
+    public class WeatherArea
+    {
+        public string Latitude { get; set; }
+
+        public string Longitude { get; set; }
+
+    }
+}
